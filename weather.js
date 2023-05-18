2e8673348c82868cdcd36c901c1a25733604ffb0
YourBrother <yourbr0ther.tv@gmail.com>
2023-05-18 15:23:20 -0400

    updated the images

--- weather.js
+++ weather.js
@@ -17,19 +17,18 @@ async function fetchWeatherData() {
         const oneCallResponse = await axios.get(oneCallUrl);
 
         const currentWeather = {
-            temperatureC: oneCallResponse.data.current.temp,
             temperatureF: (oneCallResponse.data.current.temp * 9 / 5) + 32,
             description: oneCallResponse.data.current.weather[0].description,
             rain: oneCallResponse.data.current.rain ? (oneCallResponse.data.current.rain['1h'] * 100) + '%' : 'No rain',
         };
 
         // Get the high and low temperature for the day
-        const highTemp = oneCallResponse.data.daily[0].temp.max;
-        const lowTemp = oneCallResponse.data.daily[0].temp.min;
+        const highTemp = (oneCallResponse.data.daily[0].temp.max  * 9 / 5) + 32;
+        const lowTemp = (oneCallResponse.data.daily[0].temp.min  * 9 / 5) + 32;
 
         const dailyWeather = {
-            highTemp: (highTemp * 9 / 5) + 32,  // Convert high temperature to Fahrenheit
-            lowTemp: (lowTemp * 9 / 5) + 32,
+            highTemp: highTemp,
+            lowTemp: lowTemp,
             hourlyForecast: oneCallResponse.data.hourly.map(hourlyData => ({
                 precipitation: hourlyData.precipitation ? hourlyData.precipitation['1h'] : 0
             }))
@@ -56,6 +55,8 @@ async function fetchWeatherData() {
             dailyWeather,
             chanceOfRain,
             timestamp: new Date(),
+            highTemp,
+            lowTemp
         };
 
         console.log('Weather data:', JSON.stringify(weatherData));
@@ -68,92 +69,111 @@ async function fetchWeatherData() {
 
 // Function to suggest clothing options based on the weather data
 function suggestClothing(weatherData) {
-    const { currentWeather, chanceOfRain } = weatherData;
+    const { highTemp, lowTemp, chanceOfRain } = weatherData;
 
     const recommendations = {
         outerwear: [],
         tops: [],
         bottoms: [],
-        footwear: [],
+        shoes: [],
     };
 
     const images = {
         outerwear: [],
         tops: [],
         bottoms: [],
-        footwear: [],
+        shoes: [],
     };
 
     const clothingImages = {
+        'no jacket': '/images/no_jacket.png',
         'raincoat': '/images/raincoat.png',
         'light jacket': '/images/light_jacket.png',
         'heavy jacket': '/images/heavy_jacket.png',
-        'warm sweater': '/images/warm_sweater.png',
         'long-sleeve shirt': '/images/long_sleeve_shirt.png',
         'short-sleeve shirt': '/images/short_sleeve_shirt.png',
         'pants': '/images/pants.png',
         'shorts': '/images/shorts.png',
+        'tennis shoes': '/images/tennis_shoes.png',
         'rain boots': '/images/rain_boots.png',
-        'closed-toe shoes': '/images/closed_toe_shoes.png',
         'N/A': '/images/na.png'
     };
 
-    // Outerwear suggestions
-    if (chanceOfRain > 50) {
-        recommendations.outerwear.push('raincoat');
-        images.outerwear.push(clothingImages['raincoat']);
-    } else if (currentWeather.temperatureC >= 0 && currentWeather.temperatureC < 10) {
-        recommendations.outerwear.push('light jacket');
-        images.outerwear.push(clothingImages['light jacket']);
-    } else if (currentWeather.temperatureC < 0) {
-        recommendations.outerwear.push('heavy jacket');
-        images.outerwear.push(clothingImages['heavy jacket']);
-    } else {
-        recommendations.outerwear.push('N/A');
-        images.outerwear.push(clothingImages['N/A']);
+    console.log(`High Temp: ` + highTemp)
+    console.log(`Low Temp: ` + lowTemp)
+
+    // Outerwear, tops, bottoms, and shoes suggestions
+    if (highTemp > 75 && lowTemp > 65) {
+        if (chanceOfRain > 50) {
+            recommendations.outerwear.push('raincoat');
+            recommendations.tops.push('short-sleeve shirt');
+            recommendations.bottoms.push('shorts');
+            recommendations.shoes.push('rain boots');
+        } else {
+            recommendations.outerwear.push('no jacket');
+            recommendations.tops.push('short-sleeve shirt');
+            recommendations.bottoms.push('shorts');
+            recommendations.shoes.push('tennis shoes');
+        }
+    } else if (highTemp <= 75 && lowTemp <= 65 && highTemp > 60 && lowTemp > 50) {
+        if (chanceOfRain > 50) {
+            recommendations.outerwear.push('raincoat');
+            recommendations.tops.push('short-sleeve shirt');
+            recommendations.bottoms.push('shorts');
+            recommendations.shoes.push('rain boots');
+        } else {
+            recommendations.outerwear.push('light jacket');
+            recommendations.tops.push('short-sleeve shirt');
+            recommendations.bottoms.push('shorts');
+            recommendations.shoes.push('tennis shoes');
+        }
+    } else if (highTemp <= 60 && lowTemp <= 50 && highTemp > 45 && lowTemp > 35) {
+        if (chanceOfRain > 50) {
+            recommendations.outerwear.push('raincoat');
+            recommendations.tops.push('long-sleeve shirt');
+            recommendations.bottoms.push('pants');
+            recommendations.shoes.push('rain boots');
+        } else {
+            recommendations.outerwear.push('light jacket');
+            recommendations.tops.push('long-sleeve shirt');
+            recommendations.bottoms.push('pants');
+            recommendations.shoes.push('tennis shoes');
+        }
+    } else if (highTemp <= 45 && lowTemp <= 35) {  // updated threshold for heavy jacket
+        if (chanceOfRain > 50) {
+            recommendations.outerwear.push('raincoat');
+            recommendations.tops.push('long-sleeve shirt');
+            recommendations.bottoms.push('pants');
+            recommendations.shoes.push('rain boots');
+        } else {
+            recommendations.outerwear.push('heavy jacket');
+            recommendations.tops.push('long-sleeve shirt');
+            recommendations.bottoms.push('pants');
+            recommendations.shoes.push('tennis shoes');
+        }
+
     }
 
-    // Tops suggestions
-    if (chanceOfRain > 50) {
-        recommendations.tops.push('raincoat');
-        images.tops.push(clothingImages['raincoat']);
-    } else if (currentWeather.temperatureC < 10) {
-        recommendations.tops.push('warm sweater');
-        images.tops.push(clothingImages['warm sweater']);
-    } else if (currentWeather.temperatureC >= 10 && currentWeather.temperatureC < 20) {
-        recommendations.tops.push('long-sleeve shirt');
-        images.tops.push(clothingImages['long-sleeve shirt']);
-    } else {
-        recommendations.tops.push('short-sleeve shirt');
-        images.tops.push(clothingImages['short-sleeve shirt']);
-    }
 
-    // Bottoms suggestions
-    if (chanceOfRain > 50) {
-        recommendations.bottoms.push('raincoat');
-        images.bottoms.push(clothingImages['raincoat']);
-    } else if (currentWeather.temperatureC < 15) {
-        recommendations.bottoms.push('pants');
-        images.bottoms.push(clothingImages['pants']);
-    } else {
-        recommendations.bottoms.push('shorts');
-        images.bottoms.push(clothingImages['shorts']);
-    }
-
-    // Footwear suggestions
-    if (chanceOfRain > 50) {
-        recommendations.footwear.push('rain boots');
-        images.footwear.push(clothingImages['rain boots']);
-    } else {
-        recommendations.footwear.push('closed-toe shoes');
-        images.footwear.push(clothingImages['closed-toe shoes']);
-    }
+    // Link recommendations to images
+    recommendations.outerwear.forEach((item, index) => {
+        images.outerwear.push(clothingImages[item]);
+    });
+    recommendations.tops.forEach((item, index) => {
+        images.tops.push(clothingImages[item]);
+    });
+    recommendations.bottoms.forEach((item, index) => {
+        images.bottoms.push(clothingImages[item]);
+    });
+    recommendations.shoes.forEach((item, index) => {
+        images.shoes.push(clothingImages[item]);
+    });
 
     console.log('Clothing recommendations:', recommendations);
     console.log('Clothing images:', images);
     return { recommendations, images };
-}
 
+}
 // Fetch weather data every hour and suggest clothing options
 setInterval(async () => {
     const weatherData = await fetchWeatherData();
