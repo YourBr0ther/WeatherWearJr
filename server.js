f436d96b957f18c24365c66440b46db54de1d957
YourBrother <yourbr0ther.tv@gmail.com>
2023-05-18 21:27:30 -0400

    manfiest changes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/scripts/scripts.js', express.static(path.join(__dirname, 'public', 'scripts', 'scripts.js')));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
 app.get('/', async (req, res) => {
   const weatherData = await fetchWeatherData();
